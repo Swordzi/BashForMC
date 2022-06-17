6b65449a056f6eb04f5e39913bf9746d73ddce2c
Swordzi <github@sword.mozmail.com>
2022-06-17 21:06:16 +0300

    CMDS

--- src/main/java/vulzed/bashcmds/BashCMDS.java
+++ src/main/java/vulzed/bashcmds/BashCMDS.java
@@ -1,23 +1,18 @@
 package vulzed.bashcmds;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BashCMDS extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        getLogger().info("BashCMDS has been enabled!");
+        // This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+        this.getCommand("basic").setExecutor(new MyPluginCommandExecutor(this));
+    }
 
-    }
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(cmd.getName().equalsIgnoreCase("tutorial")) {
-            sender.sendMessage("You used /tutorial!");
-        }
-        return false;
-    }
 
     @Override
     public void onDisable() {
--- src/main/java/vulzed/bashcmds/Introduction.java
+++
@@ -1,7 +0,0 @@
-package vulzed.bashcmds;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class Introduction extends JavaPlugin {
-
-}
